f9931a11fb3c18ea67633468c66eadcda2081ee5
Nikita <43334750+NikitaMasand@users.noreply.github.com>
2019-03-23 23:46:16 +0530

    Create SkipTheWork.java

---
+++ Dynamic-Programming/SkipTheWork.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SkipTheWork {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int test_Cases = Integer.parseInt(reader.readLine());
+        while(test_Cases-->0){
+            int size = Integer.parseInt(reader.readLine());
+            String input[] = reader.readLine().split(" ");
+            int tasks[] = new int[size];
+            for(int i=0;i<size;i++)
+            {
+                tasks[i] = Integer.parseInt(input[i]);
+            }
+            int minTime = calcMin(tasks);
+            System.out.println(minTime);
+        }
+    }
+    static int calcMin(int[]tasks)
+    {
+        int incl = tasks[0];
+        int excl = 0;
+        for(int i=1; i<tasks.length; i++)
+        {
+            int incl_new = tasks[i] + Math.min(incl,excl);
+            int excl_new = incl;
+            incl = incl_new;
+            excl = excl_new;
+        }
+        return Math.min(incl,excl);
+    }
+}
